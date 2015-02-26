e9f81479f5136c29016534fd4998f9444be29c02
eltonmendes <eltonj@MENDES-MacBook.local>
2015-02-25 21:51:46 -0300

    copy

--- CustomizableCircularProgress/CircularProgressView.m
+++ CustomizableCircularProgress/CircularProgressView.m
@@ -3,7 +3,7 @@
 //  CustomizableCircularProgress
 //
 //  Created by Elton Mendes Vieira Junior on 2/25/15.
-//  Copyright (c) 2015 CIT. All rights reserved.
+//  Copyright Elton Mendes. All rights reserved.
 //
 
 #import "CircularProgressView.h"
